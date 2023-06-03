76c7fe66b08ca543f2864f56248633948e150db8
Aditya Shetty <adishetty9988@gmail.com>
2023-06-03 10:56:19 +0530

    second-part

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-let newItemsAdd = ["Buy Food", "Cook Food", "Eat Food"];
+let newFoodItemsAdd = ["Buy Food", "Cook Food", "Eat Food"];
+let newWorkItemsAdd = [];
 
 app.get('/', (req, res) => {
+    /* Start of creating current date and day */
     let currDate = new Date();
     let currDateOptions = {
         weekday: "long",
@@ -15,14 +18,26 @@ app.get('/', (req, res) => {
         year: "numeric"
     };
     let currDay = currDate.toLocaleDateString("en-US", currDateOptions);
+    /* End of creating current date and day */
 
-    res.render("todolist", {day: currDay, newItemList: newItemsAdd});
+    res.render("todolist", {title: currDay, newItemList: newFoodItemsAdd});
+});
+
+app.get('/work', (req, res) => {
+    res.render("todolist", {title: "Work List", newItemList: newWorkItemsAdd});
 });
 
 app.post('/', (req, res) => {
-    let newItem = req.body.newItemInput;
-    newItemsAdd.push(newItem);
-    res.redirect('/');
+    let newWorkItem = req.body.newItemInput;
+    let newFoodItem = req.body.newItemInput;
+    
+    if(req.body.addNewItem === "Work List") {
+        newWorkItemsAdd.push(newWorkItem);
+        res.redirect('/work');
+    } else {
+        newFoodItemsAdd.push(newFoodItem);
+        res.redirect('/');
+    }
 });
 
 app.listen(3000, () => {
